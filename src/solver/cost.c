a83f8d0ae91e5487a1b5fafc7b8dcf28228c02d6
Hugo BRETECHE <hubretec@e2r7p15.clusters.42paris.fr>
2022-01-26 14:18:36 +0100

    fixed cost() function

--- src/solver/cost.c
+++ src/solver/cost.c
@@ -6,7 +6,7 @@
 /*   By: hubretec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 14:39:15 by hubretec          #+#    #+#             */
-/*   Updated: 2022/01/25 17:29:40 by hubretec         ###   ########.fr       */
+/*   Updated: 2022/01/26 14:18:02 by hubretec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,29 +31,49 @@ int	optimize(int index, int len)
 	return (index);
 }
 
+t_pos	special(t_list *stack_a, t_list *stack_b, t_list *node)
+{
+	t_pos	pos;
+
+	if (between(stack_a, ft_lstlast(stack_a), node, 0))
+	{
+		pos.stack_a = 1;
+		pos.stack_b = optimize(ft_lstindex(node, stack_b), ft_lstsize(stack_b));
+	}
+	else
+	{
+		pos.stack_a = optimize(ft_lstindex(ft_lstmax(stack_a), stack_a) + 1,
+				ft_lstsize(stack_a));
+		pos.stack_b = optimize(ft_lstindex(node, stack_b), ft_lstsize(stack_b));
+	}
+	return (pos);
+}
+
 t_pos	get_pos(t_list *node, t_list *stack_a, t_list *stack_b)
 {
-	int		len;
 	int		rev;
-	int		index;
 	t_pos	pos;
 	t_list	*tmp;
 
-	len = ft_lstsize(stack_a);
+	rev = 0;
 	pos.stack_a = 0;
 	pos.stack_b = 0;
 	tmp = stack_a;
 	while (tmp->next)
 	{
-		if (!rev && index > len / 2)
+		if (!rev && ft_lstindex(tmp, stack_a) > ft_lstsize(stack_a) / 2)
 			rev = 1;
 		if (between(tmp, tmp->next, node, rev))
-			pos.stack_a = optimize(index + 1, len);
-		index++;
+		{
+			pos.stack_a = optimize(ft_lstindex(tmp, stack_a) + 1,
+					ft_lstsize(stack_a));
+			pos.stack_b = optimize(ft_lstindex(node, stack_b),
+					ft_lstsize(stack_b));
+			return (pos);
+		}
 		tmp = tmp->next;
 	}
-	pos.stack_b = optimize(ft_lstindex(node, stack_b), ft_lstsize(stack_b));
-	return (pos);
+	return (special(stack_a, stack_b, node));
 }
 
 t_list	*cost(t_list *stack_a, t_list *stack_b)
@@ -68,13 +88,12 @@ t_list	*cost(t_list *stack_a, t_list *stack_b)
 	while (tmp)
 	{
 		pos = get_pos(tmp, stack_a, stack_b);
-		if (!min || abs(pos.stack_a) + abs(pos.stack_b) < min_pos)
+		if (!min || abs(pos.stack_a) + abs(pos.stack_b) <= min_pos)
 		{
 			min_pos = abs(pos.stack_a) + abs(pos.stack_b);
 			min = tmp;
 		}
 		tmp = tmp->next;
 	}
-	ft_printf("pos.stack_a : %d\npos.stack_b : %d\n", pos.stack_a, pos.stack_b);
 	return (min);
 }
